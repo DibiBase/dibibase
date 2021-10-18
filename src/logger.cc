4b02fb971657324faf3a435b5b3b357ad90eaf67
Hassan El-shazly <hassanelshazly2010@gmail.com>
2021-10-18 20:07:07 +0200

    logger refactor

--- src/logger.cc
+++ src/logger.cc
@@ -3,11 +3,24 @@
 int main() {
   using namespace dibibase::util;
 
-  Logger &logger{const_cast<decltype(logger)>(Logger::make(LogLevel::INFO))};
-
-  logger.setVerbosity(LogLevel::INFO);
-
-  Logger::make().warn("This is a warning!");
+  Logger logger = Logger::make();
 
   logger.info("This is an info message.");
+  logger.notice("This is an notice message.");
+  logger.warn("This is an warning message.");
+  logger.err("This is an error message.");
+
+  logger.set_verbosity(LogLevel::WARNING);
+
+  logger.info("This should not appear.");
+  logger.notice("This should not appear.");
+  logger.warn("This should appear as warning.");
+  logger.err("This should appear as error.");
+
+  logger.set_verbosity(LogLevel::NONE);
+
+  logger.info("This should not appear.");
+  logger.notice("This should not appear.");
+  logger.warn("This should not appear.");
+  logger.err("This should not appear.");
 }
