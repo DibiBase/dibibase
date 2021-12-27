b21ee7394a8887470a393a84efbf63b402a476e4
Mariam Fahmy <mariamfahmy66@gmail.com>
2021-12-27 14:35:35 +0200

    Adding buffer interface

--- include/util/buffer.hh
+++ include/util/buffer.hh
@@ -2,9 +2,9 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <cinttypes>
 #include <cstring>
 #include <memory>
-#include <vector>
 
 #include "common.hh"
 
@@ -13,39 +13,88 @@ namespace dibibase::util {
 class DIBIBASE_PUBLIC Buffer {
 
 public:
-  Buffer();
-  virtual ~Buffer();
+  Buffer() {};
+  virtual ~Buffer() {};
 
-  virtual uint64_t get_uint64() const = 0;
+  virtual uint64_t get_uint64() = 0;
   virtual Buffer &put_uint64(uint64_t) = 0;
 
+  virtual uint32_t get_uint32() = 0;
+  virtual Buffer &put_uint32(uint32_t) = 0;
+
+  virtual uint16_t get_uint16() = 0;
+  virtual Buffer &put_uint16(uint16_t) = 0;
+
+  virtual uint8_t get_uint8() = 0;
+  virtual Buffer &put_uint8(uint8_t) = 0;
+
+  virtual int64_t get_int64() = 0;
+  virtual Buffer &put_int64(int64_t) = 0;
+
+  virtual int32_t get_int32() = 0;
+  virtual Buffer &put_int32(int32_t) = 0;
+
+  virtual int16_t get_int16() = 0;
+  virtual Buffer &put_int16(int16_t) = 0;
+
+  virtual int8_t get_int8() = 0;
+  virtual Buffer &put_int8(int8_t) = 0;
+
+  virtual std::string get_string(int size) = 0;
+  virtual Buffer &put_string(std::string) = 0;
+
   virtual size_t size() const = 0;
-  virtual const std::unique_ptr<char[]> bytes() const = 0;
+  virtual const std::unique_ptr<unsigned char[]> bytes() const = 0;
 };
 
 class DIBIBASE_PUBLIC MemoryBuffer : public Buffer {
 
 public:
-  explicit MemoryBuffer(size_t size)
-      : m_size(size), m_offset(0), m_buf(std::make_unique<char[]>(m_size)) {}
+  explicit MemoryBuffer(size_t size);
+  explicit MemoryBuffer(std::unique_ptr<unsigned char[]> buf, size_t size);
 
-  uint64_t get_uint64() const override;
-  Buffer &put_uint64(uint64_t data) override {
-    constexpr auto size = sizeof(uint64_t);
-    memcpy(m_buf.get() + m_offset, reinterpret_cast<char *>(&data), size);
-    advance(size);
-    return *this;
-  }
+  uint64_t get_uint64() override;
+
+  Buffer &put_uint64(uint64_t data) override;
+
+  uint32_t get_uint32() override;
+  Buffer &put_uint32(uint32_t data) override;
+
+  uint16_t get_uint16() override;
+  Buffer &put_uint16(uint16_t data) override;
+
+  uint8_t get_uint8() override;
+  Buffer &put_uint8(uint8_t data) override;
+
+  int64_t get_int64() override;
+  Buffer &put_int64(int64_t) override;
+
+  int32_t get_int32() override;
+  Buffer &put_int32(int32_t) override;
+
+  int16_t get_int16() override;
+  Buffer &put_int16(int16_t) override;
+
+  int8_t get_int8() override;
+  Buffer &put_int8(int8_t) override;
+
+  std::string get_string(int size) override;
+  Buffer &put_string(std::string) override;
 
   virtual size_t size() const override { return m_size; };
-  const std::unique_ptr<char[]> bytes() const override;
+  const std::unique_ptr<unsigned char[]> bytes() const override;
+
+  virtual ~MemoryBuffer() override {}
 
 private:
   void advance(size_t step) { m_offset += step; }
+  bool is_overflow(size_t size) {
+    if(size + m_offset > m_size) return true;
+    return false;
+  }
 
 private:
   size_t m_size, m_offset;
-  std::unique_ptr<char[]> m_buf;
+  std::unique_ptr<unsigned char[]> m_buf;
 };
-
 } // namespace dibibase::util
---
+++ lib/util/buffer.cc
@@ -0,0 +1,240 @@
+#include "util/buffer.hh"
+
+#include "errors.hh"
+#include <cstring>
+
+using namespace dibibase::util;
+
+MemoryBuffer::MemoryBuffer(size_t size)
+    : m_size(size), m_offset(0),
+      m_buf(std::make_unique<unsigned char[]>(m_size)) {}
+
+MemoryBuffer::MemoryBuffer(std::unique_ptr<unsigned char[]> buf, size_t size)
+    : m_size(size), m_offset(0), m_buf(std::move(buf)) {}
+
+uint64_t MemoryBuffer::get_uint64() {
+  uint64_t data = 0;
+  constexpr auto size = sizeof(uint64_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+
+  advance(size);
+  for (int i = (int)m_offset - 1; i >= (int)(m_offset - size); --i) {
+    data <<= 8;
+    data |= (uint64_t)m_buf[i];
+  }
+  return data;
+}
+
+Buffer &MemoryBuffer::put_uint64(uint64_t data) {
+  constexpr auto size = sizeof(uint64_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+  memcpy(m_buf.get() + m_offset, reinterpret_cast<unsigned char *>(&data), size);
+  advance(size);
+  return *this;
+}
+
+uint32_t MemoryBuffer::get_uint32() {
+  uint32_t data = 0;
+  constexpr auto size = sizeof(uint32_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+
+  advance(size);
+  for (int i = (int)m_offset - 1; i >= (int)(m_offset - size); --i) {
+    data <<= 8;
+    data |= (uint32_t)m_buf[i];
+  }
+  return data;
+}
+
+Buffer &MemoryBuffer::put_uint32(uint32_t data) {
+  constexpr auto size = sizeof(uint32_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+  memcpy(m_buf.get() + m_offset, reinterpret_cast<unsigned char *>(&data), size);
+  advance(size);
+  return *this;
+}
+
+uint16_t MemoryBuffer::get_uint16() {
+  uint16_t data = 0;
+  constexpr auto size = sizeof(uint16_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+
+  advance(size);
+  for (int i = (int)m_offset - 1; i >= (int)(m_offset - size); --i) {
+    data <<= 8;
+    data |= (uint16_t)m_buf[i];
+  }
+  return data;
+}
+
+Buffer &MemoryBuffer::put_uint16(uint16_t data) {
+  constexpr auto size = sizeof(uint16_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+  memcpy(m_buf.get() + m_offset, reinterpret_cast<unsigned char *>(&data), size);
+  advance(size);
+  return *this;
+}
+
+uint8_t MemoryBuffer::get_uint8() {
+  uint8_t data = 0;
+  constexpr auto size = sizeof(uint8_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+
+  advance(size);
+  data = (uint8_t)m_buf[(int)m_offset - size];
+  return data;
+}
+
+Buffer &MemoryBuffer::put_uint8(uint8_t data) {
+  constexpr auto size = sizeof(uint8_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+  memcpy(m_buf.get() + m_offset, reinterpret_cast<unsigned char *>(&data), size);
+  advance(size);
+  return *this;
+}
+
+int64_t MemoryBuffer::get_int64() {
+  int64_t data = 0;
+  constexpr auto size = sizeof(int64_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+
+  advance(size);
+  for (int i = (int)m_offset - 1; i >= (int)(m_offset - size); --i) {
+    data <<= 8;
+    data |= (int64_t)m_buf[i];
+  }
+  return data;
+}
+
+Buffer &MemoryBuffer::put_int64(int64_t data) {
+  constexpr auto size = sizeof(int64_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+  memcpy(m_buf.get() + m_offset, reinterpret_cast<unsigned char *>(&data), size);
+  advance(size);
+  return *this;
+}
+
+int32_t MemoryBuffer::get_int32() {
+  int32_t data = 0;
+  constexpr auto size = sizeof(int32_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+
+  advance(size);
+  for (int i = (int)m_offset - 1; i >= (int)(m_offset - size); --i) {
+    data <<= 8;
+    data |= (int32_t)m_buf[i];
+  }
+  return data;
+}
+
+Buffer &MemoryBuffer::put_int32(int32_t data) {
+  constexpr auto size = sizeof(int32_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+  memcpy(m_buf.get() + m_offset, reinterpret_cast<unsigned char *>(&data), size);
+  advance(size);
+  return *this;
+}
+
+int16_t MemoryBuffer::get_int16() {
+  int16_t data = 0;
+  constexpr auto size = sizeof(int16_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+
+  advance(size);
+  for (int i = (int)m_offset - 1; i >= (int)(m_offset - size); --i) {
+    data <<= 8;
+    data |= (int16_t)m_buf[i];
+  }
+  return data;
+}
+
+Buffer &MemoryBuffer::put_int16(int16_t data) {
+  constexpr auto size = sizeof(int16_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+  memcpy(m_buf.get() + m_offset, reinterpret_cast<unsigned char *>(&data), size);
+  advance(size);
+  return *this;
+}
+
+int8_t MemoryBuffer::get_int8() {
+  int8_t data = 0;
+  constexpr auto size = sizeof(int8_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+
+  advance(size);
+  data = (int8_t)m_buf[(int)m_offset - size];
+  return data;
+}
+
+Buffer &MemoryBuffer::put_int8(int8_t data) {
+  constexpr auto size = sizeof(int8_t);
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+  memcpy(m_buf.get() + m_offset, reinterpret_cast<unsigned char *>(&data), size);
+  advance(size);
+  return *this;
+}
+
+std::string MemoryBuffer::get_string(int size) {
+  if (is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+  std::string data;
+  for (int i = m_offset; i < (int)(size + m_offset); ++i) {
+    data.push_back((char)m_buf[i]);
+  }
+  advance(size);
+  return data;
+}
+
+Buffer &MemoryBuffer::put_string(std::string data) {
+  int size = data.size();
+  if(is_overflow(size)) {
+    throw "Buffer Overflow";
+  }
+  memcpy(m_buf.get() + m_offset, data.c_str(), size);
+  advance(size);
+  return *this;
+}
+
+const std::unique_ptr<unsigned char[]> MemoryBuffer::bytes() const {
+  std::unique_ptr<unsigned char[]> buf =
+      std::make_unique<unsigned char[]>(m_size);
+
+  // Copying buffer content.
+  for (int i = 0; i < (int)m_size; ++i) {
+    buf[i] = m_buf[i];
+  }
+  return buf;
+}
