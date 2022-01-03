63c0caf1e9d2bbcb3d591cb0e807f2ca9592cdc0
Ahmed Essam <Ahmedellord56@gmail.com>
2022-01-03 02:38:55 +0200

    Implement predicate operator

--- include/query/predicate-operator.hh
+++ include/query/predicate-operator.hh
@@ -20,7 +20,30 @@ public:
 public:
   PredicateOperator(Predicate predicate);
 
-  template <class T> bool evaluate(T a, T b);
+  template <class T> bool evaluate(T a, T b) {
+
+    template <class T> bool evaluate(T a, T b) {
+
+    switch (m_predicate) {
+      case Predicate::EQUALS:
+        return a==b;
+      case Predicate::NOT_EQUALS:
+        return a!=b;
+      case Predicate::LESS_THAN:
+        return a<b;
+      case Predicate::LESS_THAN_EQUALS:
+        return a<=b;
+      case Predicate::GREATER_THAN:
+        return a>b;
+      case Predicate::GREATER_THAN_EQUALS:
+        return a>=b;
+      default:
+        return false;
+    }
+
+  }
+
+}
 
 private:
   Predicate m_predicate;
