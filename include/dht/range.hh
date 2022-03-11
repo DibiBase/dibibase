955ed2adfbdabb5c3b79629388491716fd8c676c
Hassan El-shazly <hassanelshazly2010@gmail.com>
2022-03-11 17:50:53 +0200

    complete range class

--- include/dht/range.hh
+++ include/dht/range.hh
@@ -1,10 +1,5 @@
 #pragma once
 
-#include <cstdint>
-#include <memory>
-#include <string>
-#include <vector>
-
 #include "common.hh"
 
 namespace dibibase::dht {
@@ -12,7 +7,8 @@ namespace dibibase::dht {
 class DIBIBASE_PUBLIC Range {
 
 public:
-  Range(uint64_t start, uint64_t end, std::string name);
+  Range(uint64_t start, uint64_t end, std::string address)
+      : m_start(start), m_end(end), m_address(address) {}
 
   uint64_t start() const { return m_start; }
   uint64_t end() const { return m_end; }
@@ -20,8 +16,11 @@ public:
 
   bool contains(uint64_t id) const { return m_start <= id && id < m_end; }
 
-  bool operator==(const Range &other) const;
-  bool operator!=(const Range &other) const;
+  bool operator==(const Range &other) const {
+    return m_start == other.m_start && m_end == other.m_end &&
+           m_address == other.m_address;
+  }
+  bool operator!=(const Range &other) const { return !(*this == other); }
 
 private:
   uint64_t m_start;
