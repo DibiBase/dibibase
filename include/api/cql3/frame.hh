87acba1cf633f8f4153f46c905b42fa22b4464f5
AntwanEmil <antwanemile@gmail.com>
2022-03-16 19:56:13 +0200

    simple refactoring

--- include/api/cql3/frame.hh
+++ include/api/cql3/frame.hh
@@ -6,6 +6,22 @@
 
 namespace dibibase::api::cql3 {
 
+class DIBIBASE_PUBLIC Frame {
 
+public:
+  Frame(char *, int);
+  void parse();
+  char *substr(char *, int, int);
+  char version, flags, opcode;
+  unsigned char streamID[2] = {
+      '0', '0'}; // initially filled with zeroes instead of null
+
+  int max_size;
+  char *body{nullptr};
+
+private:
+  char *buffer{nullptr};
+  char length[4] = {'0', '0', '0', '0'}; // also initially filled with zeroes
+};
 
 } // namespace dibibase::api::cql3
