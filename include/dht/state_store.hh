743fd1857881abebc35c5a192b9e63a4956ac33a
Hassan El-shazly <hassanelshazly2010@gmail.com>
2022-05-21 17:59:44 +0200

    split config

--- include/dht/state_store.hh
+++ include/dht/state_store.hh
@@ -1,19 +1,17 @@
 #pragma once
 
 #include "common.hh"
-#include "range.hh"
 
+#include "config/config.hh"
 #include "db/database.hh"
 #include "streamer/client.hh"
 
 #include "yaml-cpp/yaml.h"
 
-using dibibase::db::Database;
+using namespace dibibase::db;
+using namespace dibibase::config;
 
 namespace dibibase::dht {
-
-#define LOCAL_CONFIG_FILE "./config/node.yaml"
-#define REMOTE_CONFIG_FILE "./config/remote.yaml"
 
 class DIBIBASE_PUBLIC StateStore {
 public:
@@ -22,16 +20,12 @@ public:
     return instance;
   }
 
-  std::vector<Range> available_ranges() { return m_ranges; };
-
-  std::map<std::string, std::shared_ptr<StreamerClient>> available_streamers() {
+  std::map<std::string, std::shared_ptr<StreamerClient>> available_streamers() const {
     return m_streamers;
   };
 
-  std::string local_address() { return m_local_address; }
-
-  std::optional<std::string> get_address(const uint32_t token) {
-    for (const auto &range : m_ranges) {
+  std::optional<std::string> get_address(const uint32_t token) const {
+    for (const auto &range : config.ranges()) {
       if (range.contains(token)) {
         return range.address();
       }
@@ -58,7 +52,7 @@ public:
     return it->second;
   }
 
-  std::shared_ptr<Database> get_keyspace(const std::string &keyspace) {
+  std::shared_ptr<Database> get_keyspace(const std::string &keyspace)  {
     auto it = m_keyspaces.find(keyspace);
     if (it == m_keyspaces.end()) {
       auto keyspace_ptr = make_keyspace(keyspace);
@@ -70,23 +64,8 @@ public:
 
 private:
   StateStore() {
-    logger.info("loading local configurations");
-    YAML::Node node_config = YAML::LoadFile(LOCAL_CONFIG_FILE);
-    m_local_address = node_config["address"].as<std::string>();
-    logger.info("local address: %s", m_local_address.c_str());
-
-    logger.info("loading remote configurations");
-    YAML::Node remote_config = YAML::LoadFile(REMOTE_CONFIG_FILE);
-    YAML::Node ranges = remote_config["ranges"];
-    for (const auto &range : ranges) {
-      uint32_t start = range["start"].as<uint32_t>();
-      uint32_t end = range["end"].as<uint32_t>();
-      std::string address = range["address"].as<std::string>();
-
-      m_ranges.push_back(Range(start, end, address));
-      m_streamers[address] = make_streamer(address);
-
-      logger.info("node %s, from: %9d, to: %9d", address.c_str(), start, end);
+    for (const auto &range : config.ranges()) {
+      m_streamers[range.address()] = make_streamer(range.address());
     }
   }
 
@@ -102,9 +81,8 @@ private:
 private:
   std::map<std::string, std::shared_ptr<StreamerClient>> m_streamers;
   std::map<std::string, std::shared_ptr<Database>> m_keyspaces;
-  std::string m_local_address;
-  std::vector<Range> m_ranges;
 
+  Config config = Config::instance();
   Logger logger = Logger::make();
 };
 
